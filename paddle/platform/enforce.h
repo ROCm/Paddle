2501dca99232a23ff1059416f8706a9ab23412da
sashao <Sabre.Shao@amd.com>
2017-12-06 06:23:46 +0000

    Hipify paddle/platform

--- paddle/platform/enforce.h
+++ paddle/platform/enforce.h
@@ -118,7 +118,7 @@ inline typename std::enable_if<sizeof...(Args) != 0, void>::type throw_on_error(
 
 template <typename... Args>
 inline typename std::enable_if<sizeof...(Args) != 0, void>::type throw_on_error(
-    cudaError_t e, const Args&... args) {
+    hipError_t e, const Args&... args) {
   if (UNLIKELY(e)) {
     throw thrust::system_error(e, thrust::cuda_category(),
                                string::Sprintf(args...));
